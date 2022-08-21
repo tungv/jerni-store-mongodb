139b272d297935ff66d64b82cae76c5056c07263
Tung Vu <me@tungv.com>
2022-08-21 12:25:31 +0700

    test: add spec for optimistic locking when insertMany
    Signed-off-by: Tung Vu <me@tungv.com>

--- tests/optimistic_locking.spec.ts
+++ tests/optimistic_locking.spec.ts
@@ -59,6 +59,68 @@ test("it should not apply an insertOne twice", async (t) => {
 
   const collection = store.getDriver(model);
   const result = await collection.find().toArray();
-  t.log(result);
   t.is(result.length, 3);
+});
+
+test("it should not apply an insertMany twice", async (t) => {
+  interface TestCollection {
+    id: number;
+    name: string;
+  }
+
+  const model: MongoDBModel<TestCollection> = {
+    name: "model_3",
+    version: "1",
+    transform(event: JourneyCommittedEvent) {
+      return [
+        {
+          insertMany: [
+            { id: event.id, name: "test_" + event.type + "_first_pass" },
+            { id: event.id, name: "test_" + event.type + "_second_pass" },
+          ],
+        },
+      ];
+    },
+  };
+
+  const store = await makeMongoDBStore({
+    name: "optimistic_locking",
+    dbName: "mongodb_store_driver_v4_optimistic",
+    url: "mongodb://localhost:27017",
+    models: [model],
+  });
+
+  await store.clean();
+
+  await store.handleEvents([
+    {
+      id: 1,
+      type: "event_1",
+      payload: {},
+    },
+    {
+      id: 2,
+      type: "event_2",
+      payload: {},
+    },
+    {
+      id: 2,
+      type: "event_2",
+      payload: {},
+    },
+    {
+      id: 1,
+      type: "event_1",
+      payload: {},
+    },
+    {
+      id: 3,
+      type: "event_3",
+      payload: {},
+    },
+  ]);
+
+  const collection = store.getDriver(model);
+  const result = await collection.find().toArray();
+  t.is(result.length, 6);
 });
