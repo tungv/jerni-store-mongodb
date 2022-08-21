cc8bc275d645336224656e8249c39b6bdb781e72
Tung Vu <me@tungv.com>
2022-08-21 23:29:54 +0700

    test: add spec for complex single collection readPipeline
    Signed-off-by: Tung Vu <me@tungv.com>

--- tests/readPipeline/same-collection.spec.ts
+++ tests/readPipeline/same-collection.spec.ts
@@ -68,10 +68,161 @@ test("it should allow reading data from the same collection", async (t) => {
   };
 
   const store = await makeMongoDBStore({
-    name: "test_register_models",
+    name: "test_read_pipeline",
     dbName: "mongodb_store_driver_v4_test_read_pipeline",
     url: "mongodb://localhost:27017",
     models: [model_1, model_2],
+  });
+
+  await store.clean();
+  await store.handleEvents([
+    {
+      id: 1,
+      type: "init",
+      payload: {},
+    },
+    {
+      id: 2,
+      type: "test",
+      payload: {},
+    },
+  ]);
+});
+
+test("it should clear cache when finishing an event", async (t) => {
+  t.plan(3);
+
+  const model_1: MongoDBModel<TestCollection> = {
+    name: "model_read_clear_cache_1",
+    version: "1",
+    transform(event: JourneyCommittedEvent) {
+      if (event.type === "init") {
+        return [
+          {
+            insertMany: [
+              { id: 1, name: "test-model-1--item-1" },
+              { id: 2, name: "test-model-1--item-2" },
+              { id: 3, name: "test-model-1--item-3" },
+            ],
+          },
+        ];
+      }
+
+      if (event.type === "test_1") {
+        const res = readPipeline([
+          { $match: { id: 2 } },
+          { $project: { name: 1 } },
+        ]);
+
+        t.deepEqual(res[0].name, "test-model-1--item-2");
+        return [];
+      }
+
+      if (event.type === "test_2") {
+        const res = readPipeline([
+          { $match: { id: 3 } },
+          { $project: { name: 1 } },
+        ]);
+
+        t.deepEqual(res[0].name, "test-model-1--item-3");
+        return [];
+      }
+    },
+  };
+
+  const model_2: MongoDBModel<TestCollection> = {
+    name: "model_read_clear_cache_2",
+    version: "1",
+    transform(event: JourneyCommittedEvent) {
+      if (event.type === "init") {
+        return [
+          {
+            insertMany: [
+              { id: 1, name: "test-model-2--item-1" },
+              { id: 2, name: "test-model-2--item-2" },
+              { id: 3, name: "test-model-2--item-3" },
+            ],
+          },
+        ];
+      }
+
+      if (event.type === "test_1") {
+        const res = readPipeline([
+          { $match: { id: 2 } },
+          { $project: { name: 1 } },
+        ]);
+
+        t.deepEqual(res[0].name, "test-model-2--item-2");
+        return [];
+      }
+    },
+  };
+
+  const store = await makeMongoDBStore({
+    name: "test_read_pipeline",
+    dbName: "mongodb_store_driver_v4_test_read_pipeline",
+    url: "mongodb://localhost:27017",
+    models: [model_1, model_2],
+  });
+
+  await store.clean();
+  await store.handleEvents([
+    {
+      id: 1,
+      type: "init",
+      payload: {},
+    },
+    {
+      id: 2,
+      type: "test_1",
+      payload: {},
+    },
+    {
+      id: 3,
+      type: "test_2",
+      payload: {},
+    },
+  ]);
+});
+
+test("it should allow reading in loop", async (t) => {
+  t.plan(3 + 2 + 1);
+
+  const model_1: MongoDBModel<TestCollection> = {
+    name: "model_read_loop_1",
+    version: "1",
+    transform(event: JourneyCommittedEvent) {
+      if (event.type === "init") {
+        return [
+          {
+            insertMany: [
+              { id: 1, name: "test-model-1--item-1" },
+              { id: 2, name: "test-model-1--item-2" },
+              { id: 3, name: "test-model-1--item-3" },
+            ],
+          },
+        ];
+      }
+
+      if (event.type === "test") {
+        for (let i = 0; i < 3; i++) {
+          const res = readPipeline([
+            { $match: { id: i + 1 } },
+            { $project: { name: 1 } },
+          ]);
+          t.deepEqual(res[0].name, "test-model-1--item-" + (i + 1));
+        }
+
+        return [];
+      }
+    },
+  };
+
+  const store = await makeMongoDBStore({
+    name: "test_read_pipeline",
+    dbName: "mongodb_store_driver_v4_test_read_pipeline",
+    url: "mongodb://localhost:27017",
+    models: [model_1],
   });
 
   await store.clean();
